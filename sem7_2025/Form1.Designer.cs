714701f44f5cb78ce28703178bd85091d0519e18
Paweł Łaba <labapawel@gmail.com>
2025-10-25 10:09:11 +0200

    zmiany

---
+++ sem7_2025/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace sem7_2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(451, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adresacja IP";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 75);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(436, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.zmiana_maski);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Maska sieci";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "192.168.0.1";
+            this.textBox1.TextChanged += new System.EventHandler(this.zmiana_ip);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adres IP";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 135);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(451, 291);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Opis";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "IPv4";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 436);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KalkulatorIP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ sem7_2025/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sem7_2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IPAddress num2maska(uint maskaid)
+        {
+            uint maskawartosc = 0;
+            for (byte i = 0; i <= maskaid; i++)
+            {
+                maskawartosc |= (uint)(1 << (32 - i));
+                if (i == 0) maskawartosc = 0;
+            }
+            var tabBajtow = BitConverter.GetBytes(maskawartosc);
+            Array.Reverse(tabBajtow);
+            return new IPAddress(tabBajtow);
+        }
+
+        private string IPAddressToBin(IPAddress address) {
+            byte[] adres = address.GetAddressBytes();
+
+            return string.Join(".", adres.Select(i => Convert.ToString(i, 2).PadLeft(8, '0')));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            uint maska = 0;
+            // generowanie adresów Maski 0-32
+            for (byte i = 0; i <= 32; i++) {
+               var maskaIp = num2maska((uint)i);
+                comboBox1.Items.Add("(/" + i + ") - " +maskaIp);
+            }
+
+            comboBox1.SelectedIndex = 24;
+        }
+
+        private void zmiana()
+        {
+            listView1.Items.Clear();
+            try
+            {
+                var ip = IPAddress.Parse(textBox1.Text);
+                var lvip = listView1.Items.Add("Podany Adres");
+                lvip.SubItems.Add(ip.ToString());
+                lvip.SubItems.Add(IPAddressToBin(ip));// ip to bin
+
+                var maska = num2maska((uint)comboBox1.SelectedIndex);
+                var lvmaska = listView1.Items.Add("Wybrana maska");
+                lvmaska.SubItems.Add(maska.ToString());
+                lvmaska.SubItems.Add(IPAddressToBin(maska));// ip to bin
+                
+                // (2^32-maska)-2
+                uint liczbaHostow = (uint)Math.Pow(2, (32 - comboBox1.SelectedIndex)) - 2;
+                listView1.Items.Add("Ile hostów")
+                            .SubItems.Add(liczbaHostow.ToString());
+
+            }
+            catch (Exception ex) { 
+                listView1.Items.Add("info")
+                      .SubItems.Add("Podałeś zły adres ip");
+            }
+        }
+
+        private void zmiana_ip(object sender, EventArgs e)
+        {
+            zmiana();
+        }
+
+        private void zmiana_maski(object sender, EventArgs e)
+        {
+            zmiana();
+        }
+    }
+}
